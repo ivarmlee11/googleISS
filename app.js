4a68d60277434e1246123fe633bd9f5914f5a5f8
Ivar Lee <ivarr5@gmail.com>
2016-06-01 15:35:36 -0700

    api practice

---
+++ app.js
@@ -0,0 +1,65 @@
+
+
+// Reference to the Firebase database.
+var firebase = new Firebase("https://issgooglemap.firebaseio.com");
+
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: myData.iss_position['latitude'], lng: myData.iss_position['longitude']},
+    zoom: 8
+  });
+
+  // Add marker on user click
+  map.addListener('click', function(e) {
+    firebase.push({lat: e.latLng.lat(), lng: e.latLng.lng()});
+  });
+
+  firebase.on("child_added", function(snapshot, prevChildKey) {
+    // Get latitude and longitude from the cloud.
+    var newPosition = snapshot.val();
+
+    // Create a google.maps.LatLng object for the position of the marker.
+    // A LatLng object literal (as above) could be used, but the heatmap
+    // in the next step requires a google.maps.LatLng object.
+    var latLng = new google.maps.LatLng(newPosition.lat, newPosition.lng);
+
+    // Place a marker at that location.
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  });
+}
+$('document').ready(function(){
+
+ $.ajax({
+      type: "GET",
+      url: "http://api.open-notify.org/iss-now.json",
+      dataType: "jsonp",
+      success: function (data) {
+         console.log(data.iss_position);
+         myData = data;
+         return myData;
+      },
+  });
+
+ $('#displayruns').on('click', function(e){
+  e.preventDefault();
+  console.log(myData);
+  firebase.push({lat: myData.iss_position['latitude'], lng: myData.iss_position['longitude']});
+   $.ajax({
+      type: "GET",
+      url: "http://api.open-notify.org/iss-now.json",
+      dataType: "jsonp",
+      success: function (data) {
+         console.log(data.iss_position);
+         myData = data;
+         return myData;
+      },
+  });
+  initMap();
+ })
+
+
+});
